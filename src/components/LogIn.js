4204f665e201bcd0f14ef2410463efa91938fbe6
Hossein Tarokh <mhtarokh@gmail.com>
2023-09-19 17:50:11 +0200

    Login page is made

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import style from './logIn.css';
 
 
 function App() {
     return (
-        <>
-            <h1>Log In</h1>
-            <p>enter your username</p>
-            <p>enter your password</p>
-            <button>login</button>
-        </>
+        <div className="LoginPage">
+            <h1 className={style.bigblue}>Log In</h1>
+
+            <div className="LoginBoxContainer">
+                <label className="LoginBoxLabels" for="uname"><b>Username</b></label>
+                <input className="LoginBoxInputs" type="text" placeholder="Enter Username" name="uname" required />
+                <label className="LoginBoxLabels" for="psw"><b>Password</b></label>
+                <input className="LoginBoxInputs" type="password" placeholder="Enter Password" name="psw" required />
+                <button className="LoginBoxButton" type="submit">Login</button>
+                <div className="LoginBoxLabels">
+                    <label> <b>Remember me</b></label>
+                    <input type="checkbox" checked="checked" name="remember" placeholder="Remember me" />
+                </div>
+                
+            </div>
+
+        </div>
     )
 }
 
